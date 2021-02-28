d560348a463234eb01b17b7d594ce0eef8091927
Abhyuday Tripathi <tripathi.abhyuday2010@gmail.com>
2021-02-28 21:09:20 +0530

    Initial commit

---
+++ script.sh
@@ -0,0 +1,8 @@
+# ! /opt/homebrew/bin/zsh
+
+read -p "Enter your name: " NAME
+echo "Hello ${NAME}, !Nice to meet you"
+echo "If you are a JavaScript Coder then you will understand what I am saying to you!!"
+echo "Ok enough talking !"
+echo "Go to Hell. Use bash!!"
+bash
